5682308159ee5069585749a9534018ae45839aaf
j-ra1n <j_ra1n@naver.com>
2024-04-09 21:33:10 +0900

    두 개 뽑아서 더하기

---
+++ 프로그래머스/배열/두개뽑아서더하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class 두개뽑아서더하기 {
+
+    public int[] solution(int[] numbers) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().sorted().mapToInt(x -> x).toArray();
+    }
+
+}
