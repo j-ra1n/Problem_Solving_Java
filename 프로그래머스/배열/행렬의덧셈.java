bd03b4f7d1f851109cdadf79890afdb01ed1810d
j-ra1n <j_ra1n@naver.com>
2024-04-09 22:51:09 +0900

    행렬의 덧셈

---
+++ 프로그래머스/배열/행렬의덧셈.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class 행렬의덧셈 {
+
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+
+        int row = arr1.length;
+        int col = arr1[0].length;
+
+        int[][] result = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                result[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        return result;
+    }
+}
