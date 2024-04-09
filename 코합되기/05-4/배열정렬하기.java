efc5c47e94875537240df4fdb32d5e5dfdbf7381
j-ra1n <j_ra1n@naver.com>
2024-04-09 19:17:36 +0900

    #1

---
+++ 코합되기/05-4/배열정렬하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class 배열정렬하기 {
+
+    private static int[] solution(int arr[]) {
+
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int numbers[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int sortNumbers[] = solution(numbers);
+
+        sc.close();
+
+        System.out.println(Arrays.toString(sortNumbers));
+    }
+
+}
