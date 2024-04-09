6b3644caefbb5d7e6b70c39a2e165b2017d49678
j-ra1n <j_ra1n@naver.com>
2024-04-09 19:27:56 +0900

    테스트

---
+++ 코합되기/스택/Bj.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Bj {
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        System.out.println(Arrays.toString(arr));
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("apply", 1);
+        map.put("refuse", 2);
+        map.put("approve", 3);
+
+        System.out.println(map);
+    }
+
+}
+
+    
