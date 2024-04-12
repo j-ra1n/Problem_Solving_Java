f1b218af74dfdc06f2cc7edf3ace3b0d423d83e5
j-ra1n <j_ra1n@naver.com>
2024-04-12 14:49:11 +0900

    배열의 평균값

---
+++ 프로그래머스/배열/배열의평균값.java
@@ -0,0 +1,10 @@
+public class 배열의평균값 {
+
+    public double solution(int[] numbers) {
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum / numbers.length;
+    }
+}
