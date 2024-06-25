6ce2c897fdbfa629f5b6c3893aea30a32cbb846c
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2024-06-25 11:53:05 +0900

    [level 0] Title: 옹알이 (1), Time: 0.14 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+            
+        for(int i=0; i<babbling.length; i++)
+        {
+            babbling[i] = babbling[i].replace("aya"," ");
+            babbling[i] = babbling[i].replace("ye"," ");
+            babbling[i] = babbling[i].replace("woo"," ");
+            babbling[i] = babbling[i].replace("ma"," ");
+            babbling[i] = babbling[i].replace(" ","");
+        }
+        
+        for(int i=0; i<babbling.length; i++)
+        {
+            if(babbling[i].length()==0)
+            {
+                answer++;
+            }
+        }
+        
+    
+        return answer;
+    }
+}
