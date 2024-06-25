a1d125e27d6a63c2840f8a1209bd7ac0ae2e13b0
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2024-06-25 11:34:37 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int angle) {
+        
+        if(angle==90)
+        {
+            return 2;
+        }else if(angle==180)
+        {
+            return 4;
+        }else if(angle>0&&angle<90)
+        {
+            return 1;
+        }else
+        {
+            return 3;
+        }
+    }
+}
