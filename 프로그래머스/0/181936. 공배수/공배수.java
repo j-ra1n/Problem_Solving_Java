af668343fc869844936b9c5532266ac84b83a806
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2024-07-01 21:21:08 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        
+        if(number%n==0 && number%m==0)
+        {
+            return 1;
+        }
+        int answer = 0;
+        return answer;
+    }
+}
