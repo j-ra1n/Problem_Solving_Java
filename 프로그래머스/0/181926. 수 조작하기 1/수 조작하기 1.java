96fd354d984e2e21ef8b4f3e320328b2479f5a45
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2024-07-02 21:33:38 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.52 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        
+        for(int i=0; i<control.length(); i++)
+        {
+            char c = control.charAt(i);
+            if(c=='w')
+            {
+                answer ++;
+            }else if(c=='s')
+            {
+                answer --;
+            }else if(c=='d')
+            {
+                answer += 10;
+            }else
+            {
+                answer -=10;
+            }
+        }
+        return answer;
+    }
+}
