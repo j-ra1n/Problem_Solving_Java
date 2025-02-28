b553d8ebab07cd80166e2d2ce01b22a346e6ed85
JoungWoo Lee <118893707+j-ra1n@users.noreply.github.com>
2025-02-28 14:13:57 +0900

    [level 3] Title: 최고의 집합, Time: 1.77 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    
+
+    
+    public int[] solution(int n, int s) {
+        
+        int[] answer = new int[n];
+        
+       if(n>s)
+       {
+           return new int[]{-1};
+       }
+        
+        
+        for(int i=0; i<n; i++)
+        {
+            answer[i] = s/n;
+        }
+        
+        if(s%n>0)
+        {
+            for(int i=0; i<s%n; i++)
+            {
+                answer[i]++;
+            }
+            
+            Arrays.sort(answer);
+        }
+        
+        
+        return answer;
+    }
+}
